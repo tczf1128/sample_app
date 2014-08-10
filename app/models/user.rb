b63cb5b6e939bb4756cf203c67a1dc03152c949d
tczf <tczf1128@gmail.com>
2014-08-10 17:13:04 +0800

    Add post list in home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,13 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    microposts
+  end
+
   private
 
-    def create_remember_token
-      self.remember_token = User.hash(User.new_remember_token)
-    end
+  def create_remember_token
+    self.remember_token = User.hash(User.new_remember_token)
+  end
 end
